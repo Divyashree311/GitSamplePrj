0034c7f86491c5b58045a133bdf02aa6f4e7f94e
Divya nisarga <divyashree.kn311@gmail.com>
2021-05-10 15:59:20 +0530

    First commit

---
+++ src/com/sampleprj/Example_One.java
@@ -0,0 +1,17 @@
+package com.sampleprj;
+
+public class Example_One {
+
+	public static void main(String[] args) {
+
+		int age = 18;
+		
+		if(age<18) {
+			System.out.println("Not Eligable for voting");
+		}else {
+			System.out.println("Eligable for voting");
+		}
+		
+	}
+
+}
